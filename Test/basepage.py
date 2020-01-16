307cc16af05931ae50b24b54a5814ba1bcfd4440
Aergey <serzh.bessonov@gmail.com>
2020-01-16 19:06:48 +0300

    create_base_structure

---
+++ Test/basepage.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('/home/sergey/Documents/chromedriver')
+driver.get("https://ya.ru/")
+
+# element = driver.find_element_by_class_name('hpuQDe')
+# element.click()
